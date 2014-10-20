c40d2a770974b644f6fdf9bbca7aafa39a3715d0
igrief15 <igrief15@punahou.edu>
2014-10-20 12:38:37 -1000

    GriefNim
    i wanted the class to be my name please thank you

---
+++ GriefNim.java
@@ -0,0 +1,22 @@
+//Isaiah Grief
+public class GriefNim{
+  public GriefNim(){
+  }
+    public GriefNim(int startNumPieces){
+  }
+  
+  public int play(int pieces){
+    if ((pieces-2)%3==0) //if computer is on one of two winning values
+      return 1; //it needs to take 1
+    else if ((pieces-3)%3==0)//if computer is on the second of two winning values
+      return 2; //it needs to take 2
+    else //if computer is on a losing number 
+      return 2; //i guess he can return 2, its an arbitrary value because he will lose anyway
+  }
+}
+
+//if pieces-1 % 3 == 0, you will lose 
+//might as well choose randomly between 1 and 2
+//4,7,10,13,16, etc, are losing values
+//so if statements for whether pieces - 2 % 3 == 0 or pieces - 3 % 3 == 0 and choose
+//and then subtract 1 or 2 accordingly
